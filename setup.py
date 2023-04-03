ea54d42d042405d3cd84d17d9d050ea1407b3795
tianyic <tianyi@uw.edu>
2023-04-03 03:59:37 +0000

    update setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ reqs = parse_requirements('requirements.txt')
 
 VERSION = '2.0.9'
 DESCRIPTION = 'Only Train Once (OTO): Automatic One-Shot General DNN Training and Compression Framework'
-LONG_DESCRIPTION = 'Only Train Once (OTO): Automatic One-Shot General DNN Training and Compression Framework'
+LONG_DESCRIPTION = 'Only Train Once (OTO): Automatic One-Shot General DNN Training and Compression (via Structured Pruning) Framework'
 
 setup(
     name="only_train_once",
