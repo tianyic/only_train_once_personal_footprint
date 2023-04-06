31266886708d11112c5a977a52b2c4d70c4882a4
tianyic <tianyi@uw.edu>
2023-04-06 03:26:17 +0000

    fix

--- only_train_once/compression/compression.py
+++ only_train_once/compression/compression.py
@@ -72,14 +72,15 @@ def assign_onnx_tensors_on_oto(oto_graph, onnx_graph):
         if not matched:
             remaining_tensors.append(tensor)
 
-    conv_param_count = 0
+    # Tackle the Conv-BN fusion
+    conv_param_idx = -1
     for tensor in remaining_tensors:
         if "Conv" not in tensor.name and "conv" not in tensor.name:
             continue
         # print(f"Tensor Name: {tensor.name}, Data Type: {tensor.data_type}, Shape: {tensor.dims}")
-        idx = conv_param_count // 2
-        conv_param_count += 1
-        conv_bn_fuse = oto_graph.fused_conv_bns[idx]
+        if len(tensor.dims) == 4:
+            conv_param_idx += 1
+        conv_bn_fuse = oto_graph.fused_conv_bns[conv_param_idx]
         cc_id = conv_bn_fuse[0].cc_id
         cc = oto_graph.connected_components[cc_id]
         cc.onnx_params.add(tensor.name)
