7a7434a23fb98c4967caf6052f495f9ad419effa
tianyic <tianyi@uw.edu>
2023-04-06 02:54:17 +0000

    fix bug

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def parse_requirements(filename):
 
 reqs = parse_requirements('requirements.txt')
 
-VERSION = '2.0.9'
+VERSION = '2.0.10'
 DESCRIPTION = 'Only Train Once (OTO): Automatic One-Shot General DNN Training and Compression Framework'
 LONG_DESCRIPTION = 'Only Train Once (OTO): Automatic One-Shot General DNN Training and Compression (via Structured Pruning) Framework'
 
